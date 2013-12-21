28ba2686418f58c1a2e8b634af2fe19c81a4f554
James Quacinella <james@deemok.com>
2013-12-21 18:58:50 -0500

    Basic interface is now working, with no D3 output yet. Just outputs the rows.

--- web/applications/is602_project/controllers/default.py
+++ web/applications/is602_project/controllers/default.py
@@ -1,3 +1,5 @@
+import types
+
 def index():
 
     db['cancer_data_by_area'].AREA.requires = IS_IN_SET(states, multiple=False)
@@ -14,10 +16,7 @@ def index():
 
     db['cancer_data_by_area'].SITE.requires = IS_IN_SET(cancer_sites, multiple=False)
     db['cancer_data_by_area'].SITE.widget = SQLFORM.widgets.options.widget
-
-    db['cancer_data_by_area'].YEAR.requires = IS_IN_SET(years, multiple=False)
-    db['cancer_data_by_area'].YEAR.widget = SQLFORM.widgets.options.widget
-
+  
     db['cancer_data_by_area'].AGE_ADJUSTED_CI_LOWER.writable = False
     db['cancer_data_by_area'].AGE_ADJUSTED_CI_LOWER.readable = False
     db['cancer_data_by_area'].AGE_ADJUSTED_CI_UPPER.writable = False
@@ -34,23 +33,43 @@ def index():
     db['cancer_data_by_area'].COUNT.readable = False
     db['cancer_data_by_area'].POPULATION.writable = False
     db['cancer_data_by_area'].POPULATION.readable = False
+    db['cancer_data_by_area'].YEAR.writable = False
+    db['cancer_data_by_area'].YEAR.readable = False
 
-    cancer_form = SQLFORM.factory(db.cancer_data_by_area, _formname='cancer_form')
+    cancer_form = SQLFORM.factory(db.cancer_data_by_area, 
+                            Field('YEARS',
+                                requires=IS_IN_SET(years, multiple = True),
+                                widget = SQLFORM.widgets.multiple.widget),
+                            _formname='cancer_form')
 
     # from gluon.debug import dbg
     # dbg.set_trace() # stop here!
 
-    output = "lame"
-    if cancer_form.process(_formname='cancer_form').accepted:
+    output = None
+    query = None
+
+    def validate_cancer_form(form):
+        if not form.vars.YEARS:
+            form.errors.YEARS = 'Must provide a year!'
+
+    if cancer_form.process(_formname='cancer_form', onvalidation=validate_cancer_form).accepted:
+
         area_query = db.cancer_data_by_area['AREA'] == request.vars['AREA']
+        event_query = db.cancer_data_by_area['EVENT_TYPE'] == request.vars['EVENT_TYPE']
         race_query = db.cancer_data_by_area['RACE'] == request.vars['RACE']
         sex_query  = db.cancer_data_by_area['SEX']  == request.vars['SEX']
         site_query = db.cancer_data_by_area['SITE'] == request.vars['SITE']
-        year_query = db.cancer_data_by_area['YEAR'] == request.vars['YEAR']
         
-        output = db.cancer_data_by_area(area_query & race_query & sex_query & site_query & year_query)
+        if type(request.vars['YEARS']) is types.ListType:
+            request.vars['YEARS'] = map(int, request.vars['YEARS'])
+            year_query = reduce(lambda a,b: a|b, [db.cancer_data_by_area.YEAR==year for year in request.vars['YEARS']])
+        elif type(request.vars['YEARS']) is types.StringType:
+            year_query = db.cancer_data_by_area.YEAR==int(request.vars['YEARS'])
+
+        query = area_query & race_query  & sex_query & event_query & site_query & year_query
+        output = db(query).select()
 
     elif cancer_form.errors:
-        output = cancer_form.errors
+        session.flash = cancer_form.errors
 
     return locals()
